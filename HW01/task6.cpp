3e0494512aee11c09a838770e0cfb885990b5efa
shiggavi <shiggavi@wisc.edu>
2024-09-16 20:07:31 -0500

    Add files via upload

--- HW01/task6.cpp
+++ HW01/task6.cpp
@@ -3,7 +3,7 @@
 #include <cstdio>  // For printf
 
 int main(int argc, char* argv[]) {
-    // Check if the user has provided a command line argument
+    // For command line argument
     if (argc != 2) {
         std::cerr << "Usage: ./task6 N\n";
         return 1;
@@ -12,20 +12,20 @@ int main(int argc, char* argv[]) {
     // Convert the command line argument to an integer
     int N = std::atoi(argv[1]);
 
-    // Print from 0 to N using printf
+    // 0 to N using printf
     for (int i = 0; i <= N; ++i) {
-        printf("%d", i);
+        std::printf("%d", i);
         if (i < N) {
-            printf(" "); // Print space between numbers
+            std::printf(" "); 
         }
     }
-    printf("\n");
+    std::printf("\n");
 
-    // Print from N to 0 using std::cout
+    //N to 0 using std::cout
     for (int i = N; i >= 0; --i) {
         std::cout << i;
         if (i > 0) {
-            std::cout << " "; // Print space between numbers
+            std::cout << " "; 
         }
     }
     std::cout << std::endl;
