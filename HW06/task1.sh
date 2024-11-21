7abc03aa5f2c1360a36726ea82502d500764681d
ADITYA SHIGGAVI <shiggavi@euler-login-2.engr.wisc.edu>
2024-11-21 00:07:16 -0600

    HW06

--- HW06/task1.sh
+++ HW06/task1.sh
@@ -1,18 +1,14 @@
 #! /usr/bin/env zsh
-#SBATCH --partition=instruction   
-#SBATCH --job-name=Task1          
-#SBATCH --output="task1.out"   
-#SBATCH --error="task1.err"    
-#SBATCH --gres=gpu:1              
-#SBATCH --time=0-00:07:00         
+#SBATCH --partition=instruction
+#SBATCH --job-name=FirstSlurm
+#SBATCH --output="task1.out"
+#SBATCH --error="task1.err"
+#SBATCH --gres=gpu:1
+#SBATCH --time=0-00:30:00
 
-# Load the CUDA module
 module load nvidia/cuda/11.8.0
-
-# Compile the CUDA program with specified flags
 nvcc task1.cu matmul.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std c++17 -o task1
-
 for i in $(seq 0 9);
-do
-  ./task1 $((2**(5+i))) 1024
+do 	
+  ./task1 $((2**(5+i))) 1024
 done
--- HW06/task1_512.sh
+++ HW06/task1_512.sh
@@ -1,19 +1,14 @@
 #! /usr/bin/env zsh
 #SBATCH --partition=instruction
-#SBATCH --job-name=Task1
+#SBATCH --job-name=FirstSlurm
 #SBATCH --output="task1_512.out"
 #SBATCH --error="task1_512.err"
 #SBATCH --gres=gpu:1
-#SBATCH --time=0-00:07:00
+#SBATCH --time=0-00:30:00
 
-# Load the CUDA module
 module load nvidia/cuda/11.8.0
-
-# Compile the CUDA program with specified flags
 nvcc task1.cu matmul.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std c++17 -o task1
-
 for i in $(seq 0 9);
-do
-  ./task1 $((2**(5+i))) 512
+do 	
+  ./task1 $((2**(5+i))) 512
 done
-
