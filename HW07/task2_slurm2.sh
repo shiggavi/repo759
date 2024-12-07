a30a90fc698a37831477ba01017b1d67a53fa62c
ADITYA SHIGGAVI <shiggavi@euler-login-1.engr.wisc.edu>
2024-12-06 22:24:43 -0600

    plot

---
+++ HW07/task2_slurm2.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env zsh
+#SBATCH --partition=instruction
+#SBATCH --job-name=Task2_512
+#SBATCH --output="task2_512.out"
+#SBATCH --error="task2_512.err"
+#SBATCH --gres=gpu:1
+#SBATCH --time=0-00:30:00
+
+module load nvidia/cuda/11.8.0
+nvcc task2.cu reduce.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task2
+
+for i in $(seq 10 30); do
+  N=$((2**i))
+  ./task2 $N 512
+done
+
